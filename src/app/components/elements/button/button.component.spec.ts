6e28ac65e04368a7054430cd7201c7ca015006f3
Lyossauce <julien.mercier-chauve@hotmail.fr>
2023-05-20 11:50:24 +0200

    apply unit tests

--- src/app/components/elements/button/button.component.spec.ts
+++ src/app/components/elements/button/button.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let compiled: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +15,19 @@ describe('ButtonComponent', () => {
     });
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement;
     fixture.detectChanges();
+    fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit click event on click', () => {
+    spyOn(component.buttonClicked, 'emit');
+    let button : DebugElement = fixture.debugElement.query(By.css('button'));
+    button.nativeElement.click()
+    expect(component.buttonClicked.emit).toHaveBeenCalled();
   });
 });
--- src/app/components/pages/home/home.component.spec.ts
+++ src/app/components/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ButtonComponent } from '../../elements/button/button.component';
+import { BoardComponent } from '../board/board.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent, ButtonComponent, BoardComponent]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -18,4 +20,19 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`shouldn't show board on start but should display button`, () => {
+    component.isGameStarted = false;
+    expect(fixture.nativeElement.querySelector('app-board')).toBeFalsy();
+    expect(fixture.nativeElement.querySelector('app-button')).toBeTruthy();
+  });
+
+  it('should display board on startGame event', () => {
+    component.isGameStarted = false;
+    component.startGame();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('app-board')).toBeTruthy();
+    expect(component.isGameStarted).toBeTruthy();
+  });
+
 });
