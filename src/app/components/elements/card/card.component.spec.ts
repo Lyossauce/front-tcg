58cb26c493f41bc3a955482954f60ebdeba2c7b0
Lyossauce <julien.mercier-chauve@hotmail.fr>
2023-05-27 16:40:43 +0200

    add test for player, card and button components

--- src/app/components/elements/card/card.component.spec.ts
+++ src/app/components/elements/card/card.component.spec.ts
@@ -12,10 +12,19 @@ describe('CardComponent', () => {
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.card = "1";
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display mana and damage', () => {
+    fixture.detectChanges();
+    let mana = fixture.nativeElement.querySelector('.mana');
+    let damage = fixture.nativeElement.querySelector('.damage');
+    expect(mana.textContent).toBe("1");
+    expect(damage.textContent).toBe("1");
+  })
 });
